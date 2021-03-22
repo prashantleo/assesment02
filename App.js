8279d8fbf3e599ccec4b4cb176e29a96cfcd1836
prashantpandey4017 <prashantpandey4017@gmail.com>
2021-03-22 02:05:49 +0000

    styling

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { Component } from 'react';
+import { Container, Header, Tab, Tabs, TabHeading, Icon, Text } from 'native-base';
+import CameraPage from './Tabs/Camera.page'
+import Tab2 from './Tabs/Tab2';
+import Tab3 from './Tabs/Tab3';
+import Videopicker from './Tabs/Videopicker'
+export default class TabsAdvancedExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header hasTabs/>
+        <Tabs>
+          <Tab heading={ <TabHeading><Icon name="camera" /><Text>Camera</Text></TabHeading>}>
+            <CameraPage/>
+          </Tab>
+          <Tab heading={ <TabHeading><Text>No Icon</Text></TabHeading>}>
+            <Videopicker/>
+          </Tab>
+          <Tab heading={ <TabHeading><Icon name="apps" /></TabHeading>}>
+            <Tab3 />
+          </Tab>
+        </Tabs>
+      </Container>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Tabs/Camera.page.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import { Camera } from 'expo-camera';
+import { View, Text } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
+
+import styles from './style';
+import Toolbar from './toolbar.component';
+import Gallery from './gallery.component';
+
+export default class CameraPage extends React.Component {
+    camera = null;
+
+    state = {
+        captures: [],
+        capturing: null,
+        hasCameraPermission: null,
+        cameraType: Camera.Constants.Type.back,
+        flashMode: Camera.Constants.FlashMode.off,
+    };
+
+    setFlashMode = (flashMode) => this.setState({ flashMode });
+    setCameraType = (cameraType) => this.setState({ cameraType });
+    handleCaptureIn = () => this.setState({ capturing: true });
+
+    handleCaptureOut = () => {
+        if (this.state.capturing)
+            this.camera.stopRecording();
+    };
+
+    handleShortCapture = async () => {
+        const photoData = await this.camera.takePictureAsync();
+        this.setState({ capturing: false, captures: [photoData, ...this.state.captures] })
+        const asset = await MediaLibrary.createAssetAsync(photoData.uri);
+        console.log('asset', asset);
+        MediaLibrary.createAlbumAsync('Expo', asset)
+          .then(() => {
+            console.log('Album created!')
+          })
+          .catch(error => {
+            console.log('An Error Occurred!')
+          });
+    };
+
+    handleLongCapture = async () => {
+        const videoData = await this.camera.recordAsync();
+        this.setState({ capturing: false, captures: [videoData, ...this.state.captures] });
+       // MediaLibrary.saveToLibraryAsync(videoData.uri)
+        const asset = await MediaLibrary.createAssetAsync(videoData.uri);
+    console.log('asset', asset);
+    MediaLibrary.createAlbumAsync('Expo', asset)
+      .then(() => {
+        console.log('Album created!')
+      })
+      .catch(error => {
+        console.log('An Error Occurred!')
+      });
+  };
+    
+
+    async componentDidMount() {
+        const camera = await Permissions.askAsync(Permissions.CAMERA);
+        const audio = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+        const hasCameraPermission = (camera.status === 'granted' && audio.status === 'granted');
+
+        this.setState({ hasCameraPermission });
+        this.getCameraRollPermissions();
+
+    };
+    
+  async getCameraRollPermissions() {
+   // const { Permissions } = Expo;
+    const {status} = await Permissions.askAsync(Permissions.MEDIA_LIBRARY_WRITE_ONLY);
+    const statusMedia = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
+    if (status === 'granted' && statusMedia.status === 'granted') {
+      this.setState({ rollGranted: true });
+    } else {
+      console.log('Uh oh! The user has not granted us permission.');
+      this.setState({ rollGranted: false });
+    }
+  }
+
+    render() {
+        const { hasCameraPermission, flashMode, cameraType, capturing, captures } = this.state;
+
+        if (hasCameraPermission === null) {
+            return <View />;
+        } else if (hasCameraPermission === false) {
+            return <Text>Access to camera has been denied.</Text>;
+        }
+
+        return (
+            <React.Fragment>
+                <View>
+                    <Camera
+                        type={cameraType}
+                        flashMode={flashMode}
+                        style={styles.preview}
+                        ref={camera => this.camera = camera}
+                    />
+                </View>
+
+                {captures.length > 0 && <Gallery captures={captures}/>}
+
+                <Toolbar 
+                    capturing={capturing}
+                    flashMode={flashMode}
+                    cameraType={cameraType}
+                    setFlashMode={this.setFlashMode}
+                    setCameraType={this.setCameraType}
+                    onCaptureIn={this.handleCaptureIn}
+                    onCaptureOut={this.handleCaptureOut}
+                    onLongCapture={this.handleLongCapture}
+                    onShortCapture={this.handleShortCapture}
+                />
+            </React.Fragment>
+        );
+    };
+};
